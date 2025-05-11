be01c62d69b5e8e694d9d4346ce4019df8021c77
ViacheslavOl <162608546+ViacheslavOl@users.noreply.github.com>
2025-05-11 20:18:29 +0200

    api

--- src/api/apiRequest.js
+++ src/api/apiRequest.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3003';
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3003';
 
 export async function apiRequest(endpoint, method = 'GET', body = null, options = {}) {
   const { headers = {}, withCredentials = false } = options;
