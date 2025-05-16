e2f008cfd1602d666a80c11be482c4f0731bd9ad
ViacheslavOl <162608546+ViacheslavOl@users.noreply.github.com>
2025-05-16 19:49:03 +0200

    front

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/Modern-normalize.min.css'
-import App from './pages/App/App'
-import LandingMain from './pages/Landing/LandingMain/LandingMain';
 import Main from './main';
 import { AuthProvider } from './AuthContext.tsx';
 import { GoogleOAuthProvider } from '@react-oauth/google'
