e2242c3f28b8ac20471b85cd5a61a9aa2718e507
ViacheslavOl <162608546+ViacheslavOl@users.noreply.github.com>
2025-05-19 20:59:01 +0200

    new google login

---
+++ src/components/GoogleSignInButton.tsx
@@ -0,0 +1,30 @@
+
+import React from "react";
+import { auth } from "../firebaseConfig";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+export function GoogleSignInButton() {
+  const handleSignIn = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const idToken = await result.user.getIdToken();
+
+
+      await fetch(`${process.env.REACT_APP_API_URL}/auth/firebase`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${idToken}`,
+        },
+        credentials: "include",
+      });
+
+
+    } catch (err) {
+      console.error("Google Sign-In error:", err);
+    }
+  };
+
+  return <button onClick={handleSignIn}>Войти через Google</button>;
+}
