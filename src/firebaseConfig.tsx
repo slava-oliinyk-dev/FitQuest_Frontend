2cf9b13ddad361f60a0022c47bb90a0d65477efb
ViacheslavOl <162608546+ViacheslavOl@users.noreply.github.com>
2025-05-20 16:43:03 +0200

    front firebase

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -2,10 +2,10 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyBZ8VK2LRrrUHOK1lVLzrmqPpRVFl1doOk',
+	apiKey: 'AIzaSyBZ8VK2LRrrUHOK1lVLzrmqPpRVFI1doOk',
 	authDomain: 'fitquest-a3ae4.firebaseapp.com',
 	projectId: 'fitquest-a3ae4',
-	storageBucket: 'fitquest-a3ae4.appspot.com',
+	storageBucket: 'fitquest-a3ae4.firebasestorage.app',
 	messagingSenderId: '686300008573',
 	appId: '1:686300008573:web:f94fd779dcd6b710dba114',
 	measurementId: 'G-G4ZDF73HRZ',
