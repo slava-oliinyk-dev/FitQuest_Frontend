51e6115a216d72c3b33652cfc09983f36ea4902c
ViacheslavOl <162608546+ViacheslavOl@users.noreply.github.com>
2025-05-20 15:55:34 +0200

    front nginx

--- src/components/GoogleSignInButton/GoogleSignInButton.tsx
+++ src/components/GoogleSignInButton/GoogleSignInButton.tsx
@@ -1,28 +1,25 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from '../../firebaseConfig.tsx'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../../firebaseConfig.tsx';
 
 export function GoogleSignInButton() {
-  const handleSignIn = async () => {
-    try {
-      const provider = new GoogleAuthProvider();
-      const result = await signInWithPopup(auth, provider);
-      const idToken = await result.user.getIdToken();
+	const handleSignIn = async () => {
+		try {
+			const provider = new GoogleAuthProvider();
+			const result = await signInWithPopup(auth, provider);
+			const idToken = await result.user.getIdToken();
 
+			await fetch(`${process.env.REACT_APP_API_URL}/auth/firebase`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${idToken}`,
+				},
+				credentials: 'include',
+			});
+		} catch (err) {
+			console.error('Google Sign-In error:', err);
+		}
+	};
 
-      await fetch(`${process.env.REACT_APP_API_URL}/auth/firebase`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${idToken}`,
-        },
-        credentials: "include",
-      });
-
-
-    } catch (err) {
-      console.error("Google Sign-In error:", err);
-    }
-  };
-
-  return <button onClick={handleSignIn}>Войти через Google</button>;
+	return <button onClick={handleSignIn}>Войти через Google</button>;
 }
