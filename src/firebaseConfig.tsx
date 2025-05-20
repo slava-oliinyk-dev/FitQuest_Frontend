822e611f992fca91303ceb58067b6cb82c117a8c
ViacheslavOl <162608546+ViacheslavOl@users.noreply.github.com>
2025-05-20 16:05:58 +0200

    front nginx

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-apiKey: 'AIzaSyBZ8VK2LRrrUHOK1lVLzrmqPpRVFl1doOk',
-authDomain: 'fitquest-a3ae4.firebaseapp.com',
-projectId: 'fitquest-a3ae4',
-storageBucket: 'fitquest-a3ae4.appspot.com',
-messagingSenderId: '686300008573',
-appId: '1:686300008573:web:f94fd779dcd6b710dba114',
-measurementId: 'G-G4ZDF73HRZ',
+	apiKey: 'AIzaSyBZ8VK2LRrrUHOK1lVLzrmqPpRVFl1doOk',
+	authDomain: 'fitquest-a3ae4.firebaseapp.com',
+	projectId: 'fitquest-a3ae4',
+	storageBucket: 'fitquest-a3ae4.appspot.com',
+	messagingSenderId: '686300008573',
+	appId: '1:686300008573:web:f94fd779dcd6b710dba114',
+	measurementId: 'G-G4ZDF73HRZ',
 };
 
 const app = initializeApp(firebaseConfig);
