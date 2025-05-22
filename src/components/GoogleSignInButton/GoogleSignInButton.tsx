447e4e0454d4d640cad96f9905bef6562e332f59
ViacheslavOl <162608546+ViacheslavOl@users.noreply.github.com>
2025-05-22 18:41:01 +0200

    fix safari 2205 deleted popup

--- src/components/GoogleSignInButton/GoogleSignInButton.tsx
+++ src/components/GoogleSignInButton/GoogleSignInButton.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { GoogleAuthProvider, setPersistence, signInWithPopup, signInWithRedirect, getRedirectResult, browserLocalPersistence } from 'firebase/auth';
+import { GoogleAuthProvider, setPersistence, signInWithRedirect, getRedirectResult, browserLocalPersistence } from 'firebase/auth';
 import { auth } from '../../firebaseConfig.tsx';
 import { useNavigate } from 'react-router-dom';
 
@@ -21,21 +21,8 @@ export function GoogleSignInButton() {
 	}, []);
 
 	const handleSignIn = () => {
-		const ua = navigator.userAgent;
-		const isSafari = ua.includes('Safari') && !ua.includes('Chrome') && !ua.includes('Chromium') && !ua.includes('Android') && !ua.includes('CriOS') && !ua.includes('FxiOS');
-
 		const provider = new GoogleAuthProvider();
-
-		if (isSafari) {
-			signInWithRedirect(auth, provider);
-		} else {
-			signInWithPopup(auth, provider)
-				.then(async (result) => {
-					const idToken = await result.user.getIdToken();
-					window.location.href = `${API}/users/firebase-redirect` + `?token=${idToken}` + `&redirect=${encodeURIComponent(window.location.origin + REDIRECT_AFTER)}`;
-				})
-				.catch(console.error);
-		}
+		signInWithRedirect(auth, provider);
 	};
 
 	return (
