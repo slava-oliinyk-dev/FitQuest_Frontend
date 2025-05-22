71c58756b06e7494ab88c2261d2cf1fc53440124
ViacheslavOl <162608546+ViacheslavOl@users.noreply.github.com>
2025-05-22 18:36:50 +0200

    2205 fix problem with google auth safari 3

--- src/components/GoogleSignInButton/GoogleSignInButton.tsx
+++ src/components/GoogleSignInButton/GoogleSignInButton.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from 'react';
 import { GoogleAuthProvider, setPersistence, signInWithPopup, signInWithRedirect, getRedirectResult, browserLocalPersistence } from 'firebase/auth';
 import { auth } from '../../firebaseConfig.tsx';
-import { useAuth } from '../../AuthContext.tsx';
 import { useNavigate } from 'react-router-dom';
 
 export function GoogleSignInButton() {
-	const { setUser } = useAuth();
 	const navigate = useNavigate();
 	const API = process.env.REACT_APP_API_URL!;
 	const REDIRECT_AFTER = '/app';
@@ -22,24 +20,21 @@ export function GoogleSignInButton() {
 			.catch(console.error);
 	}, []);
 
-	const handleSignIn = async () => {
-		const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+	const handleSignIn = () => {
+		const ua = navigator.userAgent;
+		const isSafari = ua.includes('Safari') && !ua.includes('Chrome') && !ua.includes('Chromium') && !ua.includes('Android') && !ua.includes('CriOS') && !ua.includes('FxiOS');
+
 		const provider = new GoogleAuthProvider();
 
 		if (isSafari) {
-			try {
-				await signInWithRedirect(auth, provider);
-			} catch (err) {
-				console.error('Ошибка при signInWithRedirect в Safari:', err);
-			}
+			signInWithRedirect(auth, provider);
 		} else {
-			try {
-				const result = await signInWithPopup(auth, provider);
-				const idToken = await result.user.getIdToken();
-				window.location.href = `${API}/users/firebase-redirect` + `?token=${idToken}` + `&redirect=${encodeURIComponent(window.location.origin + REDIRECT_AFTER)}`;
-			} catch (err) {
-				console.error(err);
-			}
+			signInWithPopup(auth, provider)
+				.then(async (result) => {
+					const idToken = await result.user.getIdToken();
+					window.location.href = `${API}/users/firebase-redirect` + `?token=${idToken}` + `&redirect=${encodeURIComponent(window.location.origin + REDIRECT_AFTER)}`;
+				})
+				.catch(console.error);
 		}
 	};
 
