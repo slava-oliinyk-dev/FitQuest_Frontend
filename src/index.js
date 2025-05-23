5f0bc6ccc833985bc317ad038abe5d6dff9300f3
ViacheslavOl <162608546+ViacheslavOl@users.noreply.github.com>
2025-05-23 18:23:18 +0200

    2305 deleted GoogleAuthProvider

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './styles/Modern-normalize.min.css';
 import Main from './main';
 import { AuthProvider } from './AuthContext.tsx';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
