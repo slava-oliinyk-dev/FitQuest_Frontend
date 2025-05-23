3a8bcb33bca0b281fadeca89c987c8d22754d167
ViacheslavOl <162608546+ViacheslavOl@users.noreply.github.com>
2025-05-23 18:18:18 +0200

    2305 deleted GoogleAuthProvider

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/Modern-normalize.min.css';
 import Main from './main';
 import { AuthProvider } from './AuthContext.tsx';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<AuthProvider>
-			<GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
-				<Router>
-					<Main />
-				</Router>
-			</GoogleOAuthProvider>
+			<Router>
+				<Main />
+			</Router>
 		</AuthProvider>
 	</React.StrictMode>,
 );
