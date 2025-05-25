eacc50c0b317dc17845bab60f37ede7457434606
ViacheslavOl <162608546+ViacheslavOl@users.noreply.github.com>
2025-05-25 14:02:04 +0200

    2505 new icons

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,25 +5,16 @@ import { HashLink } from 'react-router-hash-link';
 import { useNavigate } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { useAuth } from '../../AuthContext.tsx';
+import { IoBarbellSharp } from 'react-icons/io5';
+import { AiFillHome } from 'react-icons/ai';
+import { IoIosLock } from 'react-icons/io';
+import { AiFillQuestionCircle } from 'react-icons/ai';
+import { IoPeopleCircleOutline } from 'react-icons/io5';
+import { IoInformationCircle } from 'react-icons/io5';
+import { TbLogout } from 'react-icons/tb';
 import './Sidebar.sass';
 import './SidebarMedia.sass';
-import {
-	gymIcon,
-	exitIcon,
-	helpIcon,
-	infoIcon,
-	homeIcon,
-	communityIcon,
-	gymOrangeIcon,
-	exitOrangeIcon,
-	helpOrangeIcon,
-	infoOrangeIcon,
-	homeOrangeIcon,
-	communityOrangeIcon,
-	lockIcon,
-	lockOrange,
-	logo,
-} from '../../assets/icons';
+import { logo } from '../../assets/icons';
 import { apiRequest } from '../../api/apiRequest';
 
 function Sidebar({ onSelectPrograms, selectedMenuItemId }) {
@@ -70,105 +61,88 @@ function Sidebar({ onSelectPrograms, selectedMenuItemId }) {
 	const workspaceMenuItems = [
 		{
 			id: 1,
-			icon: gymIcon,
-			activeIcon: gymOrangeIcon,
+			icon: <IoBarbellSharp style={{ width: '1.125rem', height: '1.5rem', color: 'white' }} />,
+			activeIcon: <IoBarbellSharp style={{ width: '1.125rem', height: '1.5rem', color: '#FF6600' }} />,
 			label: 'My Workout Programs',
-			iconStyle: { width: '1.5625rem', height: '1.5625rem' },
-			iconClass: 'sidebar__icon--large',
 		},
 		{
 			id: 2,
-			icon: lockIcon,
-			activeIcon: lockOrange,
+			icon: <IoIosLock style={{ width: '1.125rem', height: '1.5rem', color: 'white' }} />,
+			activeIcon: <IoIosLock style={{ width: '1.125rem', height: '1.5rem', color: '#FF6600' }} />,
 			label: 'Calendar',
 		},
 		{
 			id: 3,
-			icon: lockIcon,
-			activeIcon: lockOrange,
+			icon: <IoIosLock style={{ width: '1.125rem', height: '1.5rem', color: 'white' }} />,
+			activeIcon: <IoIosLock style={{ width: '1.125rem', height: '1.5rem', color: '#FF6600' }} />,
 			label: 'Progress',
-			iconStyle: { width: '1.1875rem', height: '1.1875rem' },
 		},
 		{
 			id: 4,
-			icon: lockIcon,
-			activeIcon: lockOrange,
+			icon: <IoIosLock style={{ width: '1.125rem', height: '1.5rem', color: 'white' }} />,
+			activeIcon: <IoIosLock style={{ width: '1.125rem', height: '1.5rem', color: '#FF6600' }} />,
 			label: 'Program Inbox',
-			iconStyle: { width: '1.1875rem', height: '1.1875rem' },
 		},
 	];
 
 	const controlPanelMenuItems = [
 		{
 			id: 5,
-			icon: homeIcon,
-			activeIcon: homeOrangeIcon,
+			icon: <AiFillHome style={{ width: '1.125rem', height: '1.5rem', color: 'white' }} />,
+			activeIcon: <AiFillHome style={{ width: '1.125rem', height: '1.5rem', color: '#FF6600' }} />,
 			label: 'Home',
 		},
 		{
 			id: 6,
-			icon: helpIcon,
-			activeIcon: helpOrangeIcon,
+			icon: <AiFillQuestionCircle style={{ width: '1.125rem', height: '1.5rem', color: 'white' }} />,
+			activeIcon: <AiFillQuestionCircle style={{ width: '1.125rem', height: '1.5rem', color: '#FF6600' }} />,
 			label: (
 				<HashLink
 					smooth
 					to="/#about"
 					scroll={(el) => {
-						setTimeout(() => {
-							el.scrollIntoView({ behavior: 'smooth', block: 'start' });
-						}, 50);
+						setTimeout(() => el.scrollIntoView({ behavior: 'smooth', block: 'start' }), 50);
 					}}
 				>
 					About the App
 				</HashLink>
 			),
-			iconClass: 'sidebar__icon--medium',
 		},
 		{
 			id: 7,
-			icon: communityIcon,
-			activeIcon: communityOrangeIcon,
+			icon: <IoPeopleCircleOutline style={{ width: '1.125rem', height: '1.5rem', color: 'white' }} />,
+			activeIcon: <IoPeopleCircleOutline style={{ width: '1.125rem', height: '1.5rem', color: '#FF6600' }} />,
 			label: 'Contact & Social',
-			iconStyle: { width: '1.4375rem', height: '1.4375rem' },
-			iconClass: 'sidebar__icon--large',
 		},
 		{
 			id: 8,
-			icon: infoIcon,
-			activeIcon: infoOrangeIcon,
+			icon: <IoInformationCircle style={{ width: '1.125rem', height: '1.5rem', color: 'white' }} />,
+			activeIcon: <IoInformationCircle style={{ width: '1.125rem', height: '1.5rem', color: '#FF6600' }} />,
 			label: 'Privacy & Legal',
 		},
 		{
 			id: 9,
-			icon: exitIcon,
-			activeIcon: exitOrangeIcon,
+			icon: <TbLogout style={{ width: '1.125rem', height: '1.5rem', color: 'white' }} />,
+			activeIcon: <TbLogout style={{ width: '1.125rem', height: '1.5rem', color: '#FF6600' }} />,
 			label: 'Log Out',
-			iconStyle: { width: '1.25rem', height: '1.25rem' },
-			iconClass: 'sidebar__icon--medium',
 			onClick: handleButtonClick,
 		},
 	];
 
 	const renderMenuItems = (items) =>
-		items.map((item) => (
-			<li
-				key={item.id}
-				className={`sidebar__menu-item 
-          ${selectedMenuItemId === item.id ? 'active' : ''}`}
-				onClick={() => {
-					if (item.onClick) {
-						item.onClick();
-					} else {
-						handleMenuItemClick(item.id);
-					}
-				}}
-			>
-				<span className={`sidebar__icon ${item.iconClass || ''}`} style={item.iconStyle}>
-					<img src={selectedMenuItemId === item.id ? item.activeIcon : item.icon} alt={`${item.label} Icon`} />
-				</span>
-				<span className="sidebar__text">{item.label}</span>
-			</li>
-		));
+		items.map((item) => {
+			const isActive = selectedMenuItemId === item.id;
+			const IconComponent = isActive ? item.activeIcon : item.icon;
+
+			return (
+				<li key={item.id} className={`sidebar__menu-item ${isActive ? 'active' : ''}`} onClick={() => (item.onClick ? item.onClick() : handleMenuItemClick(item.id))}>
+					<span className={`sidebar__icon ${item.iconClass || ''}`} style={item.iconStyle}>
+						{IconComponent}
+					</span>
+					<span className="sidebar__text">{item.label}</span>
+				</li>
+			);
+		});
 
 	return (
 		<>
